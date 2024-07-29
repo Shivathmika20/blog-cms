32b583ced3a839b9ccaafc232ff1785e8fe16a4f
Shivathmika20 <yelurishivathmika@gmail.com>
2024-07-29 14:58:44 +0530

    change: client

--- lib/client.ts
+++ lib/client.ts
@@ -2,6 +2,7 @@
 import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 export const client=createClient({
+  apiVersion: '2023-06-07',
   projectId: 'p32tl05n', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: false // `false` if you want to ensure fresh data
