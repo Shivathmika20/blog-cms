895040967ba1e634b270442fbe35b08b2b7c2256
Shivathmika20 <yelurishivathmika@gmail.com>
2024-07-29 15:19:21 +0530

    style;page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -25,11 +25,11 @@ export default async function page({params}:{params: {slug:string}}) {
   const data: user=await getData(params.slug)
   // console.log(data);
   return (
-    <div>
+    <div className=''>
       <h1 className='text-center mt-6'>
         <span className='text-xl md:text-3xl font-bold leading-8 tracking-tighter'>{data.title}</span>
       </h1>
-      <Image src={urlFor(data.blogImage).url()} alt="image" width={800} height={800} className="mt-6 rounded-md border mx-auto "/>
+      <Image src={urlFor(data.blogImage).url()} alt="image" width={800} height={800} className="mt-6 rounded-md border  mx-auto "/>
       <div className='mt-12 prose-lg prose-headings:font-bold max-w-2xl mx-auto  '>
         <PortableText value={data.content}/>
       </div>
