968d39b8a44c230b12779c2a70f2c8a7bcded093
Shivathmika20 <yelurishivathmika@gmail.com>
2024-07-29 15:20:48 +0530

    finished

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -29,7 +29,7 @@ export default async function page({params}:{params: {slug:string}}) {
       <h1 className='text-center mt-6'>
         <span className='text-xl md:text-3xl font-bold leading-8 tracking-tighter'>{data.title}</span>
       </h1>
-      <Image src={urlFor(data.blogImage).url()} alt="image" width={800} height={800} className="mt-6 rounded-md border  mx-auto "/>
+      <Image src={urlFor(data.blogImage).url()} alt="image" width={800} height={800} className="mt-6 rounded-md border h-60 mx-auto "/>
       <div className='mt-12 prose-lg prose-headings:font-bold max-w-2xl mx-auto  '>
         <PortableText value={data.content}/>
       </div>
